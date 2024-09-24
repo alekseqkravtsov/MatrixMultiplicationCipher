c7657017e4f5aef83f395292361000e5b6eba786
alekseqkravtsov <alekseyhamow@gmail.com>
2024-09-24 14:00:29 +0700

    Создание проекта

---
+++ MatrixMultiplicationCipher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplicationCipher
+{
+    internal class Cryptography
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
